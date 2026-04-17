cb3670c90c50b73e49f297692ff507bb85b551a0
Elie Zheng <164921043+karmaelien@users.noreply.github.com>
2026-04-17 19:08:50 +0000

    topic exchange consumer implemented

--- tech_lab_on_campus/market_watch/topic_exchange/consume.py
+++ tech_lab_on_campus/market_watch/topic_exchange/consume.py
@@ -23,6 +23,7 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
+    bindingKey = "#." + sector + ".#"
     
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
@@ -35,5 +36,7 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    sector = sys.arv[1]
+    queueName = sys.argv[2]
 
-    sys.exit(main(sector,queue))
+    sys.exit(main(sector,queueName))
